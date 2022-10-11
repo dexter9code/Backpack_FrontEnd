523ddeb22da0ff4a25986bd3a708f4400088d98e
rudy <doomdexter07@gmail.com>
2022-10-12 01:02:43 +0530

    created header of home_screen

---
+++ src/components/SectionWrapper.js
@@ -0,0 +1,9 @@
+import styles from "./Section.module.css";
+
+const SectionWrapper = function () {
+  <div className={`${styles.section} ${styles.banner}`}>
+    <h1>hello</h1>
+  </div>;
+};
+
+export default SectionWrapper;
---
+++ src/components/screens/landing/HeaderSection.js
@@ -0,0 +1,30 @@
+import styles from "./Header.module.css";
+import Lottie from "lottie-react";
+import travleAnimaiton from "../../assets/svg/travler_walking.json";
+
+const HeaderSection = function () {
+  return (
+    <section className={styles.root__section}>
+      <div className={styles.text__container}>
+        <h1>
+          backpack <br />
+          we take care <br />
+          of your tip
+        </h1>
+        <p>
+          plan and book your perfect trip with backpack
+          <br />
+          here we take all of your headache so all you have to worry about is
+          creating new memories
+        </p>
+
+        <button className={styles.home__btn}>Explore</button>
+      </div>
+      <div className={styles.animation__container}>
+        <Lottie animationData={travleAnimaiton} loop={true} autoPlay />
+      </div>
+    </section>
+  );
+};
+
+export default HeaderSection;
